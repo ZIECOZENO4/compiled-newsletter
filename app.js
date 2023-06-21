3767306984715df3fa1b5f77aba069c2e337c4fc
ZIECOZENO4 <ziecozeno4@gmail.com>
2023-06-21 21:40:44 +0200

    Lastest modified all

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.post("/", function (req, res) {
         });
 
         if (response.statusCode === 200) {
+            console.log(err);
             res.sendFile(__dirname + "/success.html");
         } else {
             res.sendFile(__dirname + "/failure.html");
